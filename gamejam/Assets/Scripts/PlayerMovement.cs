a5b9553228e14854fa2ea268a9dd2c47e48a3132
FendiAH <fendiabouhassan@gmail.com>
2021-11-12 04:21:14 +0200

    Game Jam
    Press shift for Dash and you wont be able to dash again until 3 seconds have passed

--- gamejam/Assets/Scripts/PlayerMovement.cs
+++ gamejam/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float jumpForce = 8f;
 
+    private Camera cam;
+
     [SerializeField]
     Transform groundCheck;
 
@@ -20,10 +22,32 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody player;
 
+    [SerializeField]
+    private float dashSpeed = 100f;
+
+    [SerializeField]
+    private float dashCooldown = 0f;
+
+    [SerializeField]
+    public bool canSlow = false;
+
+    [SerializeField]
+    public int backPack = 0;
+
+    [SerializeField]
+    public float healthTimer = 300f;
+
+    [SerializeField]
+    public float powerTimer = 0f;
+
+    private AudioClip timeTick;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody>();
+
+        //    cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -39,11 +63,60 @@ public class PlayerMovement : MonoBehaviour
             player.AddForce(new Vector3(player.velocity.x, jumpForce, player.velocity.z), ForceMode.Impulse);
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldown <= 0f)
+        {
+            player.velocity = new Vector3(horizontalInput * dashSpeed, player.velocity.y, verticalInput * dashSpeed);
+            dashCooldown = 3f;
+        }
+        if (dashCooldown > 0)
+        {
+            dashCooldown -= Time.deltaTime;
+            Debug.Log(dashCooldown);
+        }
+
+        if (healthTimer > 0)
+        {
+            healthTimer -= Time.deltaTime;
+        }
     }
+
+    //private void OnGUI()
+    //{
+    //    Vector3 point = new Vector3();
+    //    Event currentEvent = Event.current;
+    //    Vector2 mousePos = new Vector2();
+
+    //    mousePos.x = currentEvent.mousePosition.x;
+    //    mousePos.y = currentEvent.mousePosition.y;
+
+    //    point = cam.ScreenToViewportPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+    //    GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+    //    GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
+    //    GUILayout.Label("Mouse position: " + mousePos);
+    //    GUILayout.Label("World position: " + point.ToString("F3"));
+    //    GUILayout.EndArea();
+    //}
 
     bool IsGrounded()
     {
         return Physics.CheckSphere(groundCheck.position, .1f, ground);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            healthTimer -= 2.0f;
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            healthTimer -= 4.0f;
+        }
+        else if (collision.gameObject.tag == "Item")
+        {
+            powerTimer += 6.0f;
+        }
     }
 }
 
